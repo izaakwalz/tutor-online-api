d6e179c5c78f696966584a2496d5c94eae021969
izaakwalz <iblack.xyz@gmail.com>
2021-07-08 12:26:51 +0100

    changed progress route to user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
 
-const { UpdateData, DeleteData } = require('../controllers/user.controller');
-const { getGradeBYID } = require('../controllers/progress.controller');
+const {
+  GetData,
+  UpdateData,
+  MakeMeATutor,
+} = require('../controllers/user.controller');
+const {
+  addProgress,
+  GetTestScore,
+  getGradeBYID,
+} = require('../controllers/progress.controller');
 const { protect } = require('../middlewares/auth.middleware');
 
-router.route('/').get(protect, getGradeBYID);
-router.route('/:id').put(UpdateData).delete(DeleteData);
-// router.route('/user').get(protect, getGradeBYID);
+router.route('/').get(protect, GetData).put(protect, UpdateData);
+
+router.route('/grade').get(protect, getGradeBYID);
+
+router
+  .route('/grade/:id')
+  .post(protect, addProgress)
+  .get(protect, GetTestScore);
+
+router.route('/make-me-a-tutor').put(protect, MakeMeATutor);
 
 module.exports = router;
